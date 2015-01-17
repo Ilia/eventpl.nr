4d9bada11d910d9c28c9cc13d1234f53ce9a3024
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2015-01-17 14:24:15 +1100

    [SERVER] - add show to appointment

--- server/app/routes.php
+++ server/app/routes.php
@@ -1,11 +1,10 @@
 <?php
 
-Route::group(array('prefix' => 'api/v1'), function()
-{
+Route::group(array('prefix' => 'api/v1'), function() {
   Route::resource('calendars', 'CalendarController', 
       array('only' => array('index', 'update', 'store', 'destroy', 'show')));
   Route::resource('calendars.appointments', 'AppointmentController',
-      array('only' => array('index', 'update', 'store', 'destroy')));
+      array('only' => array('index', 'update', 'store', 'destroy', 'show')));
 });
 
 // TODO: define missing properly
