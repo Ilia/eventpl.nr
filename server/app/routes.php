c382a19ac4836a8f159fda27e4d8eaedcb588ea2
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2015-01-18 18:19:50 +1100

    [SERVER] better urls

--- server/app/routes.php
+++ server/app/routes.php
@@ -1,13 +1,8 @@
 <?php
 
-Route::group(array('prefix' => 'api/v1'), function() {
+Route::group(array('prefix' => 'v1'), function() {
   Route::resource('calendars', 'CalendarController', 
       array('only' => array('index', 'update', 'store', 'destroy', 'show')));
   Route::resource('calendars.appointments', 'AppointmentController',
       array('only' => array('index', 'update', 'store', 'destroy', 'show')));
-});
-
-// TODO: define missing properly
-App::missing(function($exception) { 
-    return View::make('index'); 
 });
