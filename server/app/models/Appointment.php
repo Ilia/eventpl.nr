a31258642bf5ba8cb3f2de6ddcf81c47180de680
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2015-01-19 21:34:23 +1100

    [SERVER] use new validation

--- server/app/models/Appointment.php
+++ server/app/models/Appointment.php
@@ -7,9 +7,16 @@ class Appointment extends Model {
   protected static $_rules = [
     'title' => 'required',
     'start_datetime' => 'required',
-    'end_datetime' => 'required'
+    'end_datetime' => 'required',
+    'start_datetime'   => 'required|datetime:Y-m-d H:i:s|after:yesterday',
+    'end_datetime'     => 'required|datetime:Y-m-d H:i:s|after:yesterday|after:start_datetime'
   ];
 
+  protected static $_messages = [
+    'end_datetime' => 'End time must be after start time',
+    'datetime' => "Please provide a valid date [Y-m-d H:i:s]"
+  ];
+  
   protected $fillable = array('title', 'notes', 'location', 'start_datetime', 
                               'end_datetime');
 
