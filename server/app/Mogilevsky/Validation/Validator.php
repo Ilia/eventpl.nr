027911723de5adbd14a9653e933f4282ae2d8b41
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2015-01-19 21:32:39 +1100

    [SERVER] new customer validation for validating datetime and start < end

---
+++ server/app/Mogilevsky/Validation/Validator.php
@@ -0,0 +1,31 @@
+<?php 
+namespace Mogilevsky\Validation;
+
+class Validator extends \Illuminate\Validation\Validator {
+
+
+  public function validateDateTime($attribute, $value, $parameters)
+  {
+      $format = 'Y-m-d H:i:s';
+      if (isset($parameters[0])){
+        $format = $parameters[0];
+      }
+      \DateTime::createFromFormat($format, trim($value));
+      $valid = \DateTime::getLastErrors();        
+      return $valid['warning_count'] == 0 && $valid['error_count'] == 0;
+  }
+  /**
+   * Check to see if end date comes after start date
+   * 
+   * @param  $attribute
+   * @param  $value
+   * @param  $parameters 
+   * @return boolean
+   */
+  public function validateAfter($attribute, $value, $parameters)
+  {
+    $start_datetime = $this->getValue($parameters[0]); 
+    return (strtotime($value) > strtotime($start_datetime));
+  }
+
+}
